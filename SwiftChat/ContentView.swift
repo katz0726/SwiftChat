efb8606ba62982398dd3f53d9b4cf22bb6c257cd
katz0726 <katz0726@gmail.com>
2024-03-29 00:29:05 +0900

    Initial Commit

---
+++ SwiftChat/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftChat
+//  
+//  Created by katz0726 on 2024/03/29
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//  
+//  Created by katz0726 on 2024/03/29
+//  
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
