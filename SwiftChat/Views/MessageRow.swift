44592eb8ea666bc0379200d63aa292d056334ceb
katz0726 <katz0726@gmail.com>
2024-03-29 01:13:53 +0900

    Split message area into another file

---
+++ SwiftChat/Views/MessageRow.swift
@@ -0,0 +1,25 @@
+//
+//  MessageRow.swift
+//  SwiftChat
+//  
+//  Created by katz0726 on 2024/03/29
+//  
+//
+
+import SwiftUI
+
+struct MessageRow: View {
+    var body: some View {
+        HStack {
+            Circle()
+                .frame(width: 60, height: 60)
+            Capsule()
+                .frame(height: 60)
+        }
+        .padding(.bottom)
+    }
+}
+
+#Preview {
+    MessageRow()
+}
