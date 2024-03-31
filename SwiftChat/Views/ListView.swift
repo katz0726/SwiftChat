ea6c86ef94db5b135c3e39982c42a0805e9117f8
katz0726 <katz0726@gmail.com>
2024-03-31 20:41:32 +0900

    add list view

---
+++ SwiftChat/Views/ListView.swift
@@ -0,0 +1,19 @@
+//
+//  ListView.swift
+//  SwiftChat
+//  
+//  Created by katz0726 on 2024/03/31
+//  
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
